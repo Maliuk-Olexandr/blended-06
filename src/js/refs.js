471aa9d8e5eab64b371653af077606911fb0f1cd
Maliuk-Olexandr <maamcd100@gmail.com>
2025-06-23 03:22:54 +0300

    feat: add DOM element references for search form and gallery

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,7 @@
 //Обʼєкт з посиланнями на ДОМ елементи
 
-export const refs = {};
+export const refs = {
+  form: document.querySelector('.search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
