9cb0ab45e0f48fff4fdaf2d29609aec962093239
Maliuk-Olexandr <maamcd100@gmail.com>
2025-06-27 21:46:58 +0300

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import { globSync } from 'glob';
+import path from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
+  // Знаходимо тільки HTML-файли в src, крім папки partials
+  const htmlFiles = globSync('./src/*.html');
+
+  const input = htmlFiles.reduce((acc, file) => {
+    const name = path.basename(file, path.extname(file)); // index, wishlist, cart
+    acc[name] = path.resolve(file);
+    return acc;
+  }, {});
+
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
@@ -13,8 +23,10 @@ export default defineConfig(({ command }) => {
     build: {
       target: 'esnext',
       sourcemap: true,
+      outDir: '../dist',
+      emptyOutDir: true,
       rollupOptions: {
-        input: glob.sync('./src/*.html'),
+        input, // тільки index, wishlist, cart
         output: {
           manualChunks(id) {
             if (id.includes('node_modules')) {
@@ -35,12 +47,10 @@ export default defineConfig(({ command }) => {
           },
         },
       },
-      outDir: '../dist',
-      emptyOutDir: true,
     },
     plugins: [
       injectHTML(),
-      FullReload(['./src/**/**.html']),
+      FullReload(['./src/**/*.html']),
       SortCss({
         sort: 'mobile-first',
       }),
